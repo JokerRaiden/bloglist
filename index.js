c2a5c02561106f2378a503031f3935d15d14cd06
JokerRaiden <joona.rahkonen@gmail.com>
2020-07-03 19:13:03 +0300

    extract app to own module

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+const server = http.createServer(app)
 
 const PORT = config.PORT
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
